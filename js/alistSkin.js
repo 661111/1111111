bca9baf82cd960c7c1d99c50aa5dec6cf05cccdc
661111 <108344166+661111@users.noreply.github.com>
2022-06-29 16:43:07 +0800

    Update alistSkin.js

--- js/alistSkin.js
+++ js/alistSkin.js
@@ -1,52 +1,98 @@
 /*
  * name : Future
  * edition : 2.0.2
- * message : 前端美化样式由星环[Future]提供，程序由Alist创始者友情提供
+ * message : 鍓嶇缇庡寲鏍峰紡鐢辨槦鐜痆Future]鎻愪緵锛岀▼搴忕敱Alist鍒涘鑰呭弸鎯呮彁渚�
  * technical support : https://www.xhto.cn/
  */
 
 var x = true;
+var conarr = ["鏄熺幆鍗氬-浜戠洏缇庡寲鍖�","https://www.xhto.cn/"];
 
 $("html").click(function() {
     if (x) {
         var title = document.title,
-            // 个人信息编写区域+星环博客提供技术支持+ —————— ON
+            // 涓汉淇℃伅缂栧啓鍖哄煙+鏄熺幆鍗氬鎻愪緵鎶€鏈敮鎸�+ 鈥斺€斺€斺€斺€斺€� ON
 
-            // 头像链接地址
+            // 澶村儚閾炬帴鍦板潃
             headimg = "https://file.xhto.cn/alistskin/img/himg.jpg",
-            // 博主铭言句子
-            motto = "不理白驹过隙，不信生死有命！",
-            // 配置 - 导航内容及链接【注：名称和链接需要一一对应，用英文逗号隔开】
-            linkname = ["首页", "星环博客","gitub"],
-            link = ["/", "https://www.xhto.cn/","https://github.com/661111/",],
+            // 鍗氫富閾█鍙ュ瓙
+            motto = "涓嶇悊鐧介┕杩囬殭锛屼笉淇＄敓姝绘湁鍛斤紒",
+            // 閰嶇疆 - 瀵艰埅鍐呭鍙婇摼鎺ャ€愭敞锛氬悕绉板拰閾炬帴闇€瑕佷竴涓€瀵瑰簲锛岀敤鑻辨枃閫楀彿闅斿紑銆�
+            linkname = ["棣栭〉", "鏄熺幆鍗氬"],
+            link = ["/", "https://www.xhto.cn/"],
             code = "";
 
-        // 个人信息编写区域+星环博客提供技术支持+ —————— END
+        // 涓汉淇℃伅缂栧啓鍖哄煙+鏄熺幆鍗氬鎻愪緵鎶€鏈敮鎸�+ 鈥斺€斺€斺€斺€斺€� END
 
-        // 程序化代码
-        code += '<h3>' + title + '</h3>';
-        code += '<div >< img src = "' + headimg + '" alt = "[ ' + title + ' ]" / >< /div>';
-        code += '<p>' + motto + '</p >';
-        code += '< ul >';
+        // 绋嬪簭鍖栦唬鐮�
+        code += '<h3>'+title+'</h3>';
+        code += '<div id="uxhdimg"><img src="'+headimg+'" alt="['+title+']" /></div>';
+        code += '<p>'+motto+'</p>';
+        code += '<ul>';
         for (vo in linkname) {
-            code += '<li> < a href = "' + link[vo] + '" > ' + linkname[vo] + ' < /a></li >';
+            code += '<li><a href="'+link[vo]+'">'+linkname[vo]+'</a></li>';
         }
-        code += '< /ul>';
+        code += '</ul>';
 
-        // 代码写入视觉程序
+        // 浠ｇ爜鍐欏叆瑙嗚绋嬪簭
         $(".header").append(code);
+        // 鍒ゆ柇鏄惁鍐欏叆
+        x = false;
+        console.log('%c '+conarr[0]+' %c '+conarr[1]+' ','padding: 5px 10px; color: #FFF; background-color:#282828; border-radius: 2px 0px 0px 2px;','padding: 5px 10px;color: #FFF; background-image: linear-gradient(to left,#00f2fe,#66ccff); border-radius: 0px 2px 2px 0px;');
     }
 })
 
-// 支持样式代码
+// 鏀寔鏍峰紡浠ｇ爜
 $(document).on('click', '.buttons>span:eq(1) svg', function() {
     var $viwe = $(this).attr('viewBox');
-    alert($viwe);
 })
 
-window.onload = function() {
+// 妫€娴嬫槸鍚︽暟鎹坊鍔犱笂
+window.onload = function(){
     setTimeout(function() {
-        $("html").click();
-        x = false;
-    }, 100);
+        if($("#uxhdimg").length <= 0){
+            x = true;
+        }
+        if(x){
+            $("html").click();
+            x = false;
+        }
+    }, 200);
+    
+    setTimeout(function() {
+        if($("#uxhdimg").length <= 0){
+            x = true;
+        }
+        if(x){
+            $("html").click();
+            x = false;
+        }
+    }, 500);
+    setTimeout(function() {
+        if($("#uxhdimg").length <= 0){
+            x = true;
+        }
+        if(x){
+            $("html").click();
+            x = false;
+        }
+    }, 1000);
+    setTimeout(function() {
+        if($("#uxhdimg").length <= 0){
+            x = true;
+        }
+        if(x){
+            $("html").click();
+            x = false;
+        }
+    }, 2000);
+    setTimeout(function() {
+        if($("#uxhdimg").length <= 0){
+            x = true;
+        }
+        if(x){
+            $("html").click();
+            x = false;
+        }
+    }, 5000);
 }
